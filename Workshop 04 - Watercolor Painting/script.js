fc64c72c52226913e7201f92fceedc9c9001ceda
PP <pipop.r@hotmail.com>
2021-10-29 18:56:03 +0700

    Update script.js

--- Workshop 04 - Watercolor Painting/script.js
+++ Workshop 04 - Watercolor Painting/script.js
@@ -2,7 +2,7 @@
     const canvas = document.getElementById('painting');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
- 
+
     const context = canvas.getContext('2d');
 
     let previousPoint = { x: 0, y: 0 };
